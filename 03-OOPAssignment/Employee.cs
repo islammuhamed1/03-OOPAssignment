76d874ffe0232a454afe3aed49a3c782583de4ce
Islam <islamcobra00@gmail.com>
2025-01-16 04:20:50 +0200

    03-OOP-Assignment

---
+++ 03-OOPAssignment/Employee.cs
@@ -0,0 +1,27 @@
+﻿using System.Globalization;
+
+namespace _03_OOPAssignment
+{
+     class Employee
+     {
+        public int ID { get; set; }
+        public string? Name { get; set; }
+        public Security Security { get; set; }
+        public decimal Salary { get; set; }
+        public HireDate? HireDate { get; set; }
+        public Gender Gender { get; set; }
+        public Employee(int id, string name, Security _security, decimal salary, HireDate hireDate, Gender gender)
+        {
+            ID = id;
+            Name = name;
+            Security = _security;
+            Salary = salary;
+            HireDate = hireDate;
+            Gender = gender;
+        }
+        public override string ToString()
+        {
+            return $"ID: {ID}, Name: {Name}, \nSecurity: {Security}, Salary: {Salary.ToString("C", CultureInfo.CurrentCulture)}, \nHire Date: {HireDate}, Gender: {Gender}";
+        }
+    }
+}
---
+++ 03-OOPAssignment/HireDate.cs
@@ -0,0 +1,25 @@
+﻿namespace _03_OOPAssignment
+{
+     class HireDate
+     {
+        public int Day { get; set; }
+        public int Month { get; set; }
+        public int Year { get; set; }
+        public HireDate(int day, int month, int year)
+        {
+            Day = day;
+            Month = month;
+            Year = year;
+        }
+        public DateTime DateTime()
+        {
+            return new DateTime(Year, Month, Day);
+        }
+        public override string ToString()
+        {
+            return $"{Day}//{Month}//{Year}";
+        }
+    }
+
+}
+
